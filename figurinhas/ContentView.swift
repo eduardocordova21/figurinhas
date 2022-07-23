145a829ffcfb2175f11412b7c27d568fdf3c0a7d
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-07-23 00:27:06 -0300

    first commit

--- figurinhas/ContentView.swift
+++ figurinhas/ContentView.swift
@@ -8,9 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var itensNaColecao: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack(alignment: .leading){
+            
+            Text("Mingle")
+                .font(.title)
+                .bold()
+                .padding()
+            
+            HStack(){
+                Text("Frase: ")
+                    .bold()
+                
+                Text("\"Problema em dobro\"")
+            }.padding()
+            
+            HStack(){
+                Image("figura_01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 75)
+                  
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: "lifepreserver")
+                            .foregroundColor(Color.green)
+                            .frame(width: 30)
+                            .font(.system(size: 30))
+                          
+                        Text("Vidas: ")
+                                                
+                        Text("2")
+                            .padding(.trailing)
+                    }
+                    
+                    HStack {
+                        Image(systemName: "bolt")
+                            .foregroundColor(Color.yellow)
+                            .frame(width: 30)
+                            .font(.system(size: 30))
+                          
+                        Text("Potencia: ")
+                        
+                        Spacer()
+                        
+                        Text("60%")
+                    }
+                }
+            }.padding(30.0)
+                 
+            HStack(){
+                Spacer()
+                
+                Text("Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extrema precisão. Essas habilidades são úteis para ela em sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.gray)
+                    
+                Spacer()
+            }
+        
+            Spacer()
+            
+            HStack(){
+                Spacer()
+                
+                Button {
+                    itensNaColecao += 1
+                } label: {
+                    
+                    if(itensNaColecao == 0){
+                        Text("Adicionar à coleção")
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .background(Color.blue)
+                    }else{
+                        Text("Na sua colecao: \(itensNaColecao)")
+                            .padding()
+                            .background(Color.green)
+                    }
+                }
+                .cornerRadius(50)
+                
+                Spacer()
+            }
             .padding()
+            
+        }
     }
 }
 
