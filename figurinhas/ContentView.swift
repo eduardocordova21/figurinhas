1169d166945850b663949c8434f3082abab8f2e4
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-07-22 22:34:55 -0300

    Initial Commit

---
+++ figurinhas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  figurinhas
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 22/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ figurinhas/figurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  figurinhasApp.swift
+//  figurinhas
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct figurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
