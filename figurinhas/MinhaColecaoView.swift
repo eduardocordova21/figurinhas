5ed48e1cfdffc08e229ddc9a7c00b0e48ef7aa57
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-08-04 01:35:20 -0300

    Adicionando detalhes da minha coleção

--- figurinhas/MinhaColecaoView.swift
+++ figurinhas/MinhaColecaoView.swift
@@ -10,15 +10,34 @@ import SwiftUI
 struct MinhaColecaoView: View {
     
     @EnvironmentObject var minhaColecao: MinhaColecao
+    @State var isPresented = false
     
     var body: some View {
         
         if(minhaColecao.figuras.count == 0){
             Text("Você não tem figuras na sua coleção!")
         }else{
+            
             List(minhaColecao.figuras){ figura in
-                Text(figura.nome)
-            }
+                
+                Button{
+                    isPresented = true
+                } label: {
+                    HStack(){
+                        Image(figura.imagem+"_p")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .background(Color.green)
+                            .containerShape(Circle())
+                        
+                        Text(figura.nome)
+                    }
+                }.sheet(isPresented: $isPresented) {
+                    FigurinhaDetalheView(figura: figura)
+                }
+               
+            }.navigationTitle("Minha coleção")
         }
     
     }
